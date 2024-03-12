77955b99cc27aa36bcdc5fe707ebc4a319976968
Edmonds-Git <edmondst10@hotmail.com>
2024-03-12 16:53:09 +0000

    V0.0.4
    Created/updated testbenches for PWM module and angle control module as well as a combined testbench.
    
    Both modules passed the testbenches so I know that the arm control issue is most likely caused by the hardware.

--- Project Files/PROJ300_Edmonds/PWM_SERVO_CONTROL_TB.sv
+++ Project Files/PROJ300_Edmonds/PWM_SERVO_CONTROL_TB.sv
@@ -9,6 +9,9 @@ module PWM_SERVO_CONTROL_TB;
 	logic [7:0]angle2;
 	logic [7:0]angle3;
 	logic [7:0]angle4;
+	//pass and fail used to view how many asserts failed
+	logic [7:0]pass = 0;
+	logic [7:0]fail = 0;
 	PWM_SERVO_CONTROL u1(servo1,servo2,servo3,servo4,clk,nextangle,angle1,angle2,angle3,angle4);
 	
 	initial 
@@ -28,6 +31,15 @@ module PWM_SERVO_CONTROL_TB;
 		#10ps;
 		nextangle = 1'd1;
 		@(posedge clk)
+		nextangle = 0;
+		@(posedge clk)
+		assert (servo1 == 0) begin pass = pass +1; $display ("pass servo1 is %d PWM is working",servo1);end else begin $error ("fail servo is %d PWM is not working", servo1); fail = fail + 1;end
+		#17602us;
+		assert (servo1 == 1) begin pass = pass +1; $display ("pass servo1 is %d PWM is working",servo1);end else begin $error ("fail servo is %d PWM is not working", servo1); fail = fail + 1;end
+		#2399us;
+		assert (servo1 == 0) begin pass = pass +1; $display ("pass servo1 is %d PWM is working",servo1);end else begin $error ("fail servo is %d PWM is not working", servo1); fail = fail + 1;end
+		@(posedge clk)
 		nextangle = 1'd0;
+		$stop;
 	end
 endmodule
