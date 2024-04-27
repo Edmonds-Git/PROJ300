577fdc8029f34a0e36211be18ef4074d76180541
Edmonds-Git <edmondst10@hotmail.com>
2024-04-27 11:52:08 +0100

    V0.7.2
    KEY_INPUT was broken because of a simple mistake, it now mostly works although the value still jumps around a bit, I think its a synchronisation issue but I'm not sure.
    Created a new ANGLE_DECODE module to simplify the KEY_INPUT module by moving some of the code.

--- Project Files/PROJ300_Edmonds/KEY_INPUT.sv
+++ Project Files/PROJ300_Edmonds/KEY_INPUT.sv
@@ -11,40 +11,40 @@ module KEY_INPUT(
 	output logic [7:0]angle1,
 	output logic [7:0]angle2,
 	output logic [7:0]angle3,
-	output logic [7:0]angle4,
-	output logic [11:0]angle_out);
+	output logic [7:0]angle4);
+//	output logic [11:0]angle_out);
 	
 	logic [3:0] switches;
 	logic [7:0] angle_1, angle_2, angle_3, angle_4;
-	logic [7:0] angle1_1, angle1_2, angle1_3, angle2_1, angle2_2, angle2_3, angle3_1, angle3_2, angle3_3, angle4_1, angle4_2, angle4_3;
-	logic [11:0] angle_out_1, angle_out_2, angle_out_3, angle_out_4;
+//	logic [7:0] angle1_1, angle1_2, angle1_3, angle2_1, angle2_2, angle2_3, angle3_1, angle3_2, angle3_3, angle4_1, angle4_2, angle4_3;
+//	logic [11:0] angle_out_1, angle_out_2, angle_out_3, angle_out_4;
 	//logic [3:0] angleswitch;
 	assign switches = {SW1,SW2,SW3,SW4};
 	
 	
-	always@(negedge KEY0 or negedge KEY3 or negedge KEY1)
+	always@(posedge clk)
 	begin
 			if (KEY0 == 0)
 			begin
 				if (switches == 4'b1000 && (angle1 < 8'd180)) //1
 				begin
 					angle_1 <= angle1+4'd1;
-					angle1 <= angle_1;
+				//	angle1 <= angle_1;
 				end
 				else if (switches == 4'b0100 && (angle2 < 8'd180)) //2
 				begin
 					angle_2 <= angle2+4'd1;
-					angle2 <= angle2;
+				//	angle2 <= angle2;
 				end
 				else if (switches == 4'b0010 && (angle3 < 8'd180)) //3
 				begin
 					angle_3 <= angle3+4'd1;
-					angle3<= angle_3;
+					//angle3<= angle_3;
 				end
 				else if (switches == 4'b0001 && (angle4 < 8'd180)) //4
 				begin
 					angle_4 <= angle4+4'd1;
-					angle4 <= angle_4;
+				//	angle4 <= angle_4;
 				end
 			end
 			
@@ -53,23 +53,37 @@ module KEY_INPUT(
 				if (switches == 4'b1000 && (angle1 > 8'd0)) //1
 				begin
 					angle_1 <= angle1-4'd1;
-					angle1 <= angle_1;
+					//angle1 <= angle_1;
 				end
 				else if (switches == 4'b0100 && (angle2 > 8'd0)) //2
 				begin
 					angle_2 <= angle2-4'd1;
-					angle2 <= angle2;
+					//angle2 <= angle2;
 				end
 				else if (switches == 4'b0010 && (angle3 > 8'd0)) //3
 				begin
 					angle_3 <= angle3-4'd1;
-					angle3<= angle_3;
+					//angle3<= angle_3;
 				end
 				else if (switches == 4'b0001 && (angle4 > 8'd0)) //4
 				begin
 					angle_4 <= angle4-4'd1;
-					angle4 <= angle_4;
+					//angle4 <= angle_4;
 				end
+			end
+			else if (KEY1 == 0)
+			begin
+				angle_1 <= 8'd90;
+				angle_2 <= 8'd90;
+				angle_3 <= 8'd90;
+				angle_4 <= 8'd90;
+			end
+			else
+			begin 
+				angle_1 <= angle_1;
+				angle_2 <= angle_2;
+				angle_3 <= angle_3;
+				angle_4 <= angle_4;
 			end
 //	begin
 //			if (KEY0 == 0)
@@ -158,78 +172,74 @@ module KEY_INPUT(
 //					angle4 = angle4-4'd1;
 //				end
 //			end
-			else if (KEY1 == 0)
-			begin
-				angle1 <= 8'd90;
-				angle2 <= 8'd90;
-				angle3 <= 8'd90;
-				angle4 <= 8'd90;
-			end
-			else
-			begin
-				angle1 <= angle1;
-				angle2 <= angle2;
-				angle3 <= angle3;
-				angle4 <= angle4;
-			end
-
-//	end
-//	
-//	always_ff@(posedge KEY0 or posedge KEY3 or negedge KEY1)
-//	begin
-//			angle1 <= angle_1;
-//			angle2 <= angle_2;
-//			angle3 <= angle_3;
-//			angle4 <= angle_4;
-//			if (KEY1 == 0)
+//			else if (KEY1 == 0)
 //			begin
 //				angle1 <= 8'd90;
 //				angle2 <= 8'd90;
 //				angle3 <= 8'd90;
 //				angle4 <= 8'd90;
 //			end
+//			else
+//			begin
+//				angle1 <= angle1;
+//				angle2 <= angle2;
+//				angle3 <= angle3;
+//				angle4 <= angle4;
+//			end
+
 	end
+//	
 	always@(posedge clk)
 	begin
-		case(switches)
-			4'b1000:
+	if (KEY0 == 1 && KEY1 == 1 && KEY3 == 1)
 			begin
-	//		if (switches == 4'b1000)
-				angle1_1 <= (angle1 / 8'd100)% 8'd10;
-				angle1_2 <= (angle1/ 8'd10) % 8'd10;
-				angle1_3 <= angle1 % 8'd10;
-				angle_out_1 <= {angle1_1[3:0], angle1_2[3:0], angle1_3[3:0]};
-				angle_out <= angle_out_1;
+				angle1 <= angle_1;
+				angle2 <= angle_2;
+				angle3 <= angle_3;
+				angle4 <= angle_4;
 			end
-	//		else if (switches == 4'b0100) //2
-			4'b0100:
-			begin
-				angle2_1 <= (angle2 / 8'd100) % 8'd10;
-				angle2_2 <= (angle2 / 8'd10) % 8'd10;
-				angle2_3 <= angle2 % 8'd10;
-				angle_out_2 <= {angle2_1[3:0], angle2_2[3:0], angle2_3[3:0]};
-				angle_out <= angle_out_2;
-			end
-	//		else if (switches == 4'b0010) //3
-			4'b0010:
-			begin
-				angle3_1 <= (angle3 / 8'd100) % 8'd10;
-				angle3_2 <= (angle3 / 8'd10) % 8'd10;
-				angle3_3 <= angle3 % 8'd10;
-				angle_out_3 <= {angle3_1[3:0], angle3_2[3:0], angle3_3[3:0]};
-				angle_out <= angle_out_3;
-			end
-	//		else if (switches == 4'b0001) //4
-			4'b0001:
-			begin
-				angle4_1 <= (angle4 / 8'd100) % 8'd10;
-				angle4_2 <= (angle4 / 8'd10) % 8'd10;
-				angle4_3 <= angle4 % 8'd10;
-				angle_out_4 <= {angle4_1[3:0], angle4_2[3:0], angle4_3[3:0]};
-				angle_out <= angle_out_4;
-			end
-			default: angle_out <= angle_out;
-		endcase
 	end
+//	always@(posedge clk)
+//	begin
+//		case(switches)
+//			4'b1000:
+//			begin
+//	//		if (switches == 4'b1000)
+//				angle1_1 <= (angle1 / 8'd100)% 8'd10;
+//				angle1_2 <= (angle1/ 8'd10) % 8'd10;
+//				angle1_3 <= angle1 % 8'd10;
+//				angle_out_1 <= {angle1_1[3:0], angle1_2[3:0], angle1_3[3:0]};
+//				angle_out <= angle_out_1;
+//			end
+//	//		else if (switches == 4'b0100) //2
+//			4'b0100:
+//			begin
+//				angle2_1 <= (angle2 / 8'd100) % 8'd10;
+//				angle2_2 <= (angle2 / 8'd10) % 8'd10;
+//				angle2_3 <= angle2 % 8'd10;
+//				angle_out_2 <= {angle2_1[3:0], angle2_2[3:0], angle2_3[3:0]};
+//				angle_out <= angle_out_2;
+//			end
+//	//		else if (switches == 4'b0010) //3
+//			4'b0010:
+//			begin
+//				angle3_1 <= (angle3 / 8'd100) % 8'd10;
+//				angle3_2 <= (angle3 / 8'd10) % 8'd10;
+//				angle3_3 <= angle3 % 8'd10;
+//				angle_out_3 <= {angle3_1[3:0], angle3_2[3:0], angle3_3[3:0]};
+//				angle_out <= angle_out_3;
+//			end
+//	//		else if (switches == 4'b0001) //4
+//			4'b0001:
+//			begin
+//				angle4_1 <= (angle4 / 8'd100) % 8'd10;
+//				angle4_2 <= (angle4 / 8'd10) % 8'd10;
+//				angle4_3 <= angle4 % 8'd10;
+//				angle_out_4 <= {angle4_1[3:0], angle4_2[3:0], angle4_3[3:0]};
+//				angle_out <= angle_out_4;
+//			end
+//			default: angle_out <= angle_out;
+//		endcase
+//	end
 	
 endmodule
